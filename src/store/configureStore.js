1fbf9df56c29ddf248346bea4bbc79293649b3ff
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-02-01 17:44:07 +0500

    redux devtools added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,23 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers";
 import logger from "redux-logger";
 import api from "../middleware/api";
+import DevTools from "../containers/DevTools";
 
 const configureStore = initialState => {
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(logger, api)
+    compose(applyMiddleware(logger, api), DevTools.instrument())
   );
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept("../reducers", () => {
+      store.replaceReducer(rootReducer);
+    });
+  }
+
   return store;
 };
 
