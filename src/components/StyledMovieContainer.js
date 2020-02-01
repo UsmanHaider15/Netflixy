8586d1dc3dbc734ba20c453273c0c4a46aa0c31d
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-02-01 23:28:13 +0500

    Maintainable Responsive Design with Styled Components

--- src/components/StyledMovieContainer.js
+++ src/components/StyledMovieContainer.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
+import { devices } from "../utils/styledUtils";
 
 const StyledMovieContainer = styled.section`
   background: ${props => props.theme.secondaryBd};
   width: 100%;
   height: 100vh;
 
-  @media screen and (min-width: 768px) {
+  ${devices.md`
     width: 640px;
     height: 390px;
-    border-radius: 15px;
-  }
+    border-radius: 15px;`};
 `;
 
 export default StyledMovieContainer;
