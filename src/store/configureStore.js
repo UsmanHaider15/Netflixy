0df7a4f29e09282d16d306a19cb9d5ec2902fcb6
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-02-01 17:25:54 +0500

    added api middlware in createStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 import logger from "redux-logger";
+import api from "../middleware/api";
 
 const configureStore = initialState => {
-  const store = createStore(rootReducer, initialState, applyMiddleware(logger));
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(logger, api)
+  );
   return store;
 };
 
