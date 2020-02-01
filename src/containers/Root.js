4310d5fca7031749b9571ea88397a97e87b2c7de
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-02-01 22:20:27 +0500

    global styles added

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,10 +3,12 @@ import { Provider } from "react-redux";
 import store from "../store";
 import DevTools from "./DevTools";
 import StyledContainer from "../components/styledContainer";
+import GlobalStyles from "../GlobalStyles";
 
 const Root = () => {
   return (
     <Provider store={store}>
+      <GlobalStyles />
       <StyledContainer>
         <DevTools />
       </StyledContainer>
